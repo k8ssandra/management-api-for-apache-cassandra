d5dadeb2b48b149639720adf8bd5b0801079b0e1
John Trimble <trimblej@gmail.com>
2020-03-05 13:25:02 -0700

    Almost there...

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -23,10 +23,9 @@ _sed-in-place() {
 }
 
 # Copy over any config files mounted at /config
-cp /config/cassandra.yaml /etc/cassandra/cassandra.yaml
-# if [ -d "/config" ] && ! [ "/config" -ef "$CASSANDRA_CONF" ]; then
-# 	cp -R /config/* "${CASSANDRA_CONF:-/etc/cassandra}"
-# fi
+if [ -d "/config" ] && ! [ "/config" -ef "$CASSANDRA_CONF" ]; then
+	cp -R /config/* "${CASSANDRA_CONF:-/etc/cassandra}"
+fi
 
 if [ -d "/opt/mgmtapi" ] ; then
     echo "Starting Management API"
