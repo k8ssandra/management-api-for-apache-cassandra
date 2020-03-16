72a029931f0bee1c73984218971700bafc24f85a
Jim Dickinson <jim.dickinson@datastax.com>
2020-03-16 09:36:04 -0400

    Switch this to mgmtapi so /var/lib/cassandra is owned by cassandra

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -8,10 +8,10 @@ if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 fi
 
 # allow the container to be started with `--user`
-if [ "$1" = 'cassandra' -a "$(id -u)" = '0' ]; then
+if [ "$1" = 'mgmtapi' -a "$(id -u)" = '0' ]; then
 	find "$CASSANDRA_CONF" /var/lib/cassandra /var/log/cassandra \
 		\! -user cassandra -exec chown cassandra '{}' +
-	exec gosu cassandra "$BASH_SOURCE" "$@"
+	# exec gosu cassandra "$BASH_SOURCE" "$@"
 fi
 
 _ip_address() {
