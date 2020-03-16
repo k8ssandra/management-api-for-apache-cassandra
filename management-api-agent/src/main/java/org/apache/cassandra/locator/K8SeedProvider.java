869ee6f5967ff59eb5b1b15b2e9d0feb268c5848
Jim Dickinson <jim.dickinson@gmail.com>
2020-03-16 14:05:54 -0400

    Cassandra 4.0 docker image, and make docker-entrypoint.sh more like upstream (#6)
    * Porting the 3.11 Dockerfile from https://github.com/docker-library/cassandra and changing it to 4.0-alpha3
    
    * docker-entrypoint.sh from upstream
    
    * Keep more of the upstream docker-entrypoint.sh
    
    * Make ENTRYPOINT and CMD more similar to upstream
    
    * Fix the K8SeedProvider constructor
    
    * Switch this to mgmtapi so /var/lib/cassandra is owned by cassandra
    
    * Don't sed in place for seeds

--- management-api-agent/src/main/java/org/apache/cassandra/locator/K8SeedProvider.java
+++ management-api-agent/src/main/java/org/apache/cassandra/locator/K8SeedProvider.java
@@ -1,11 +1,16 @@
 package org.apache.cassandra.locator;
 
 import java.util.List;
+import java.util.Map;
 
 import com.datastax.mgmtapi.ShimLoader;
 
 public class K8SeedProvider implements SeedProvider
 {
+
+    public K8SeedProvider(Map<String, String> m) {
+    }
+
     @Override
     public List getSeeds()
     {
