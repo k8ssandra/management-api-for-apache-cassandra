0a536a41ec9e4275cd8dfb8f07f6d658151ae8dd
Erik Merkle <erik.merkle@datastax.com>
2021-08-26 21:45:32 -0500

    Add per dc replication factor overrides

--- management-api-agent-common/src/main/java/com/datastax/mgmtapi/interceptors/SystemDistributedReplicationInterceptor.java
+++ management-api-agent-common/src/main/java/com/datastax/mgmtapi/interceptors/SystemDistributedReplicationInterceptor.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableMap;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.bytebuddy.agent.builder.AgentBuilder;
@@ -34,19 +35,78 @@ import static net.bytebuddy.matcher.ElementMatchers.nameEndsWith;
 
 public class SystemDistributedReplicationInterceptor
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SystemDistributedReplicationInterceptor.class);
     private static final String SYSTEM_DISTRIBUTED_NTS_DC_OVERRIDE_PROPERTY = "cassandra.system_distributed_replication_dc_names";
     private static final String SYSTEM_DISTRIBUTED_NTS_RF_OVERRIDE_PROPERTY = "cassandra.system_distributed_replication_per_dc";
+    /**
+     * This DC_RF property is used to specify different RF per DC, instead of a single RF for all DCs
+     * The format of the property value should be <dc1_name>:<dc1_rf>,<dc2_name>:<dc2_rf>,....
+     *
+     * ex.  cassandra.system_distributed_replication=dc1:1,dc2:3,dc3:3
+     *
+     * If both this override and either of the above overrides are present, this value will take
+     * precedence.
+     */
+    private static final String SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY = "cassandra.system_distributed_replication";
+
+    private static Map<String, String> parseDcRfOverrides()
+    {
+        Map<String, String> dcRfOverrides = null;
+        try
+        {
+            if (System.getProperty(SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY) != null)
+            {
+                dcRfOverrides = new HashMap<>();
+                String mappings = System.getProperty(SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY);
+                for (String mapping : mappings.split(","))
+                {
+                    String map = mapping.trim();
+                    List<String> parts = Arrays.stream(map.split(":"))
+                            .map(String::trim)
+                            .collect(Collectors.toList());
+                    if (parts.size() != 2)
+                    {
+                        LOGGER.error("Invalid dc-rf mapping for {}: {}",
+                                SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY,
+                                mapping);
+                    }
+                    else {
+                        String dc = parts.get(0);
+                        Integer rf = Integer.parseInt(parts.get(1));
+                        if (rf <= 0 || rf > 5)
+                        {
+                            LOGGER.error("Invalid repliction factor specified for {}: {}",
+                                    SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY,
+                                    mapping);
+                        }
+                        else
+                        {
+                            dcRfOverrides.put(dc, rf.toString());
+                        }
+                    }
+                }
+            }
+        }
+        catch (Throwable t)
+        {
+            LOGGER.error("Error parsing system distributed replication override properties", t);
+        }
+
+        return dcRfOverrides;
+    }
 
     private static final Map<String, String> SYSTEM_DISTRIBUTED_NTS_OVERRIDE;
     static
     {
         Integer rfOverride = null;
         List<String> dcOverride = Collections.emptyList();
+        Map<String, String> dcRfOverrides = null;
         Map<String, String> ntsOverride = new HashMap<>();
         ntsOverride.put(ReplicationParams.CLASS, NetworkTopologyStrategy.class.getSimpleName());
 
         try
         {
+            dcRfOverrides = parseDcRfOverrides();
             rfOverride = Integer.getInteger(SYSTEM_DISTRIBUTED_NTS_RF_OVERRIDE_PROPERTY, null);
             dcOverride = Arrays.stream(System.getProperty(SYSTEM_DISTRIBUTED_NTS_DC_OVERRIDE_PROPERTY, "")
                     .split(","))
@@ -55,22 +115,27 @@ public class SystemDistributedReplicationInterceptor
         }
         catch (Throwable t)
         {
-            LoggerFactory.getLogger(SystemDistributedReplicationInterceptor.class).error("Error parsing system distributed replication override properties", t);
+            LOGGER.error("Error parsing system distributed replication override properties", t);
         }
 
-        if (rfOverride != null && !dcOverride.isEmpty())
+        if (dcRfOverrides != null && !dcRfOverrides.isEmpty())
+        {
+            ntsOverride.putAll(dcRfOverrides);
+            LOGGER.info("Using override for distributed system keyspaces: {}", ntsOverride);
+        }
+        else if (rfOverride != null && !dcOverride.isEmpty())
         {
             //Validate reasonable defaults
             if (rfOverride <= 0 || rfOverride > 5)
             {
-                LoggerFactory.getLogger(SystemDistributedReplicationInterceptor.class).error("Invalid value for {}", SYSTEM_DISTRIBUTED_NTS_RF_OVERRIDE_PROPERTY);
+                LOGGER.error("Invalid value for {}", SYSTEM_DISTRIBUTED_NTS_RF_OVERRIDE_PROPERTY);
             }
             else
             {
                 for (String dc : dcOverride)
                     ntsOverride.put(dc, String.valueOf(rfOverride));
 
-                LoggerFactory.getLogger(SystemDistributedReplicationInterceptor.class).info("Using override for distributed system keyspaces: {}", ntsOverride);
+                LOGGER.info("Using override for distributed system keyspaces: {}", ntsOverride);
             }
         }
 
--- management-api-server/src/test/java/com/datastax/mgmtapi/LifecycleIT.java
+++ management-api-server/src/test/java/com/datastax/mgmtapi/LifecycleIT.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -27,6 +29,7 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.auth.AuthenticationException;
 import com.datastax.oss.driver.api.core.config.DriverConfigLoader;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.internal.core.auth.PlainTextAuthProvider;
 import org.apache.http.HttpStatus;
 
@@ -210,7 +213,87 @@ public class LifecycleIT extends BaseDockerIntegrationTest
                 ResultSet rs = session.execute(String.format("select replication from system_schema.keyspaces where keyspace_name='%s'", systemKeyspace));
 
                 Map<String, String> params = rs.one().getMap("replication", String.class, String.class);
-                assertEquals(params.get("dc1"), "1");
+                assertEquals("1", params.get("dc1"));
+            }
+
+        }
+        finally
+        {
+            //Stop before next test starts
+            boolean stopped = client.post(URI.create("http://localhost/api/v0/lifecycle/stop").toURL(), null)
+                    .thenApply(r -> r.status().code() == HttpStatus.SC_OK).join();
+
+            assertTrue(stopped);
+        }
+    }
+
+    @Test
+    public void testDcReplicationFatorOverrides() throws IOException
+    {
+        assumeTrue(IntegrationTestUtils.shouldRun());
+
+        boolean ready = false;
+        NettyHttpClient client = null;
+        try
+        {
+            client = getClient();
+
+            //Configure
+            boolean configured = client.post(URI.create( BASE_PATH + "/lifecycle/configure?profile=dcrftest").toURL(),
+                    FileUtils.readFileToString(IntegrationTestUtils.getFile(this.getClass(), "dcrf-override-1.yaml")), "application/yaml")
+                    .thenApply(r -> r.status().code() == HttpStatus.SC_OK).join();
+
+            assertTrue(configured);
+
+            //Startup
+            boolean started = client.post(URI.create( BASE_PATH + "/lifecycle/start?profile=dcrftest").toURL(), null)
+                    .thenApply(r -> r.status().code() == HttpStatus.SC_CREATED).join();
+
+            assertTrue(started);
+
+            int tries = 0;
+            while (tries++ < 10)
+            {
+                ready = client.get(URI.create(BASE_PATH + "/probes/readiness").toURL())
+                        .thenApply(r -> r.status().code() == HttpStatus.SC_OK).join();
+
+                if (ready)
+                    break;
+
+                Uninterruptibles.sleepUninterruptibly(10, TimeUnit.SECONDS);
+            }
+
+            assertTrue(ready);
+
+            //addRole
+            boolean roleAdded = client.post(URI.create(BASE_PATH + "/ops/auth/role?username=dcrftest&password=dcrftest&is_superuser=true&can_login=true").toURL(), null)
+                    .thenApply(r -> r.status().code() == HttpStatus.SC_OK).join();
+
+            // create a session
+            CqlSession session =  new TestgCqlSessionBuilder()
+                    .withConfigLoader(DriverConfigLoader.programmaticBuilder()
+                            .withString(AUTH_PROVIDER_CLASS, PlainTextAuthProvider.class.getCanonicalName())
+                            .withString(AUTH_PROVIDER_USER_NAME, "dcrftest")
+                            .withString(AUTH_PROVIDER_PASSWORD, "dcrftest")
+                            .withString(LOAD_BALANCING_LOCAL_DATACENTER, "dc1")
+                            .build())
+                    .addContactPoint(new InetSocketAddress("127.0.0.1", 9042))
+                    .build();
+
+            for (String systemKeyspace : Arrays.asList("system_auth", "system_distributed", "system_traces"))
+            {
+                ResultSet rs = session.execute(String.format("select replication from system_schema.keyspaces where keyspace_name='%s'", systemKeyspace));
+
+                List<Row> rows = rs.all();
+                Map<String, String> actual = new HashMap<>();
+                for (Row row: rows) {
+                    Map<String, String> params = row.getMap("replication", String.class, String.class);
+                    actual.putAll(params);
+                }
+                assertEquals("1", actual.get("dc1"));
+                assertEquals("3", actual.get("dc2"));
+                assertEquals("5", actual.get("dc3"));
+
             }
 
         }
