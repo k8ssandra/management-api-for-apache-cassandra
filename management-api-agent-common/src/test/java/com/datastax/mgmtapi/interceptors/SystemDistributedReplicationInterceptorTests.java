19d4609d5f7273fa996dfb9b4d2aa7d9e1d7f298
Michael Burman <michael.burman@datastax.com>
2023-01-11 17:50:38 +0200

    Unescape unicode characters in the system_distributed_replication properties

---
+++ management-api-agent-common/src/test/java/com/datastax/mgmtapi/interceptors/SystemDistributedReplicationInterceptorTests.java
@@ -0,0 +1,28 @@
+package com.datastax.mgmtapi.interceptors;
+
+import org.junit.Test;
+
+import java.util.Map;
+
+import static com.datastax.mgmtapi.interceptors.SystemDistributedReplicationInterceptor.*;
+import static org.junit.Assert.assertEquals;
+
+public class SystemDistributedReplicationInterceptorTests {
+
+    @Test
+    public void testSystemDistributedReplicationParsing() {
+        System.setProperty(SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY, "dc1:4");
+        Map<String, String> rfOverrides = SystemDistributedReplicationInterceptor.parseDcRfOverrides();
+        System.clearProperty(SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY);
+        assertEquals(1, rfOverrides.size());
+        assertEquals("4", rfOverrides.get("dc1"));
+
+        // Unicode version
+        System.setProperty(SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY, "dc\u00201:1,dc1:2");
+        rfOverrides = SystemDistributedReplicationInterceptor.parseDcRfOverrides();
+        System.clearProperty(SYSTEM_DISTRIBUTED_NTS_DC_RF_OVERRIDE_PROPERTY);
+        assertEquals(2, rfOverrides.size());
+        assertEquals("1", rfOverrides.get("dc 1"));
+        assertEquals("2", rfOverrides.get("dc1"));
+    }
+}
