52e7bb4fbc86be89e48a2a2d51451929096b449f
Michael Burman <michael.burman@datastax.com>
2023-02-01 12:22:02 +0200

    ResourceAsStream works in JVM even if getResource does not and they should use the same code. Go figure.

--- management-api-agent-common/src/main/java/io/k8ssandra/metrics/config/ConfigReader.java
+++ management-api-agent-common/src/main/java/io/k8ssandra/metrics/config/ConfigReader.java
@@ -3,26 +3,33 @@ package io.k8ssandra.metrics.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import io.k8ssandra.metrics.builder.relabel.RelabelSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class ConfigReader {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigReader.class);
+
     public static final String CONFIG_PATH_PROPERTY = "collector-config-path";
     public static final String CONFIG_PATH_DEFAULT = "/configs/metrics-collector.yaml";
 
     public static Configuration readConfig() {
         // Read first the default settings from the agent, which are prepended to the Configuration relabels and labels
-        URL resource = ConfigReader.class.getClass().getResource("/default-metric-settings.yaml");
-        String defaultSettings = resource.getFile();
+        InputStream ras = ConfigReader.class.getResourceAsStream("/default-metric-settings.yaml");
+        InputStreamReader rsr = new InputStreamReader(ras);
+        BufferedReader reader = new BufferedReader(rsr);
 
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 
         // Default Configuration
         try {
-            Configuration defaultConfig = mapper.readValue(new File(defaultSettings), Configuration.class);
+            Configuration defaultConfig = mapper.readValue(reader, Configuration.class);
             Configuration customConfig = readCustomConfig();
 
             if(customConfig != null) {
