8f52a40a995c04575eded7a4cbd0c63100156088
Erik Merkle <erik.merkle@datastax.com>
2023-08-15 13:54:11 -0500

    Fix getEndpoints Response (#343)

--- management-api-server/src/main/java/com/datastax/mgmtapi/resources/MetadataResources.java
+++ management-api-server/src/main/java/com/datastax/mgmtapi/resources/MetadataResources.java
@@ -8,6 +8,7 @@ package com.datastax.mgmtapi.resources;
 import com.datastax.mgmtapi.ManagementApplication;
 import com.datastax.mgmtapi.resources.common.BaseResources;
 import com.datastax.mgmtapi.resources.helpers.ResponseTools;
+import com.datastax.mgmtapi.resources.models.EndpointStates;
 import com.datastax.mgmtapi.resources.models.FeatureSet;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -56,8 +57,7 @@ public class MetadataResources extends BaseResources {
       content =
           @Content(
               mediaType = MediaType.APPLICATION_JSON,
-              schema = @Schema(implementation = String.class),
-              examples = @ExampleObject(value = ENDPOINTS_RESPONSE_EXAMPLE)))
+              schema = @Schema(implementation = EndpointStates.class)))
   public Response getEndpointStates() {
     return executeWithJSONResponse("CALL NodeOps.getEndpointStates()");
   }
@@ -102,51 +102,4 @@ public class MetadataResources extends BaseResources {
           return Response.ok(featureSet).build();
         });
   }
-
-  private static final String ENDPOINTS_RESPONSE_EXAMPLE =
-      "{\n"
-          + "    \"entity\": [\n"
-          + "        {\n"
-          + "            \"DC\": \"datacenter1\",\n"
-          + "            \"ENDPOINT_IP\": \"172.17.0.2\",\n"
-          + "            \"HOST_ID\": \"5d2318b0-cfec-4697-8ed2-014b5f434ae8\",\n"
-          + "            \"IS_ALIVE\": \"true\",\n"
-          + "            \"LOAD\": \"135527.0\",\n"
-          + "            \"NATIVE_ADDRESS_AND_PORT\": \"172.17.0.2:9042\",\n"
-          + "            \"NET_VERSION\": \"12\",\n"
-          + "            \"PARTITIONER\": \"org.apache.cassandra.dht.Murmur3Partitioner\",\n"
-          + "            \"RACK\": \"rack1\",\n"
-          + "            \"RELEASE_VERSION\": \"4.0.1\",\n"
-          + "            \"RPC_ADDRESS\": \"172.17.0.2\",\n"
-          + "            \"RPC_READY\": \"true\",\n"
-          + "            \"SCHEMA\": \"2207c2a9-f598-3971-986b-2926e09e239d\",\n"
-          + "            \"SSTABLE_VERSIONS\": \"big-nb\",\n"
-          + "            \"STATUS\": \"NORMAL,-2444908528344618126\",\n"
-          + "            \"STATUS_WITH_PORT\": \"NORMAL,-2444908528344618126\"\n,"
-          + "            \"TOKENS\": \"-2088271209278749360,-3055635452357284858,-4299155458037876832,-498266656764850722,-5378528265485478489,-6114443958196372130,-7190199839421951670,-8007871772034302464,-9025723176776729480,1120756638932192574,2098902091448306650,3877536392271893778,4973578292506832067,6064875403199044326,6909252050690206084,8349520280592789322\"\n"
-          + "        }\n"
-          + "    ],\n"
-          + "    \"variant\": {\n"
-          + "        \"language\": null,\n"
-          + "        \"mediaType\": {\n"
-          + "            \"type\": \"application\",\n"
-          + "            \"subtype\": \"json\",\n"
-          + "            \"parameters\": {},\n"
-          + "            \"wildcardType\": false,\n"
-          + "            \"wildcardSubtype\": false\n"
-          + "        },\n"
-          + "        \"encoding\": null,\n"
-          + "        \"languageString\": null\n"
-          + "    },\n"
-          + "    \"annotations\": [],\n"
-          + "    \"mediaType\": {\n"
-          + "        \"type\": \"application\",\n"
-          + "        \"subtype\": \"json\",\n"
-          + "        \"parameters\": {},\n"
-          + "        \"wildcardType\": false,\n"
-          + "        \"wildcardSubtype\": false\n"
-          + "    },\n"
-          + "    \"language\": null,\n"
-          + "    \"encoding\": null\n"
-          + "}";
 }
---
+++ management-api-server/src/main/java/com/datastax/mgmtapi/resources/models/EndpointStates.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Please see the included license file for details.
+ */
+package com.datastax.mgmtapi.resources.models;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class EndpointStates {
+
+  @JsonProperty(value = "entity", required = true)
+  public final List<Map<String, String>> entity;
+
+  @JsonProperty(value = "variant", required = false)
+  public final Variant variant;
+
+  @JsonProperty(value = "annotations", required = false)
+  public final List<String> annotations;
+
+  @JsonProperty(value = "mediaType", required = false)
+  public final MediaType mediaType;
+
+  @JsonProperty(value = "language", required = false)
+  public final String language;
+
+  @JsonProperty(value = "encoding", required = false)
+  public final String encoding;
+
+  @JsonCreator
+  public EndpointStates(
+      @JsonProperty("entity") List<Map<String, String>> entity,
+      @JsonProperty("variant") Variant variant,
+      @JsonProperty("annotations") List<String> annotations,
+      @JsonProperty("mediaType") MediaType mediaType,
+      @JsonProperty("language") String language,
+      @JsonProperty("encoding") String encoding) {
+    this.entity = entity;
+    this.variant = variant;
+    this.annotations = annotations;
+    this.mediaType = mediaType;
+    this.language = language;
+    this.encoding = encoding;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 5;
+    hash = 83 * hash + Objects.hashCode(this.entity);
+    hash = 83 * hash + Objects.hashCode(this.variant);
+    hash = 83 * hash + Objects.hashCode(this.annotations);
+    hash = 83 * hash + Objects.hashCode(this.mediaType);
+    hash = 83 * hash + Objects.hashCode(this.language);
+    hash = 83 * hash + Objects.hashCode(this.encoding);
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final EndpointStates other = (EndpointStates) obj;
+    if (!Objects.equals(this.entity, other.entity)) {
+      return false;
+    }
+    if (!Objects.equals(this.variant, other.variant)) {
+      return false;
+    }
+    if (!Objects.equals(this.annotations, other.annotations)) {
+      return false;
+    }
+    if (!Objects.equals(this.mediaType, other.mediaType)) {
+      return false;
+    }
+    if (!Objects.equals(this.language, other.language)) {
+      return false;
+    }
+    return Objects.equals(this.encoding, other.encoding);
+  }
+
+  public static class MediaType {
+
+    @JsonProperty(value = "type", required = false)
+    public final String type;
+
+    @JsonProperty(value = "subtype", required = false)
+    public final String subtype;
+
+    @JsonProperty(value = "parameters", required = false)
+    public final Map<String, String> parameters;
+
+    @JsonProperty(value = "wildcardType", required = false)
+    public final String wildcardType;
+
+    @JsonProperty(value = "wildcardSubtype", required = false)
+    public final String wildcardSubtype;
+
+    @JsonCreator
+    public MediaType(
+        @JsonProperty("type") String type,
+        @JsonProperty("subtype") String subtype,
+        @JsonProperty("parameters") Map<String, String> parameters,
+        @JsonProperty("wildcardType") String wildcardType,
+        @JsonProperty("wildcardSubtype") String wildcardSubtype) {
+      this.type = type;
+      this.subtype = subtype;
+      this.parameters = parameters;
+      this.wildcardType = wildcardType;
+      this.wildcardSubtype = wildcardSubtype;
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 5;
+      hash = 83 * hash + Objects.hashCode(this.type);
+      hash = 83 * hash + Objects.hashCode(this.subtype);
+      hash = 83 * hash + Objects.hashCode(this.parameters);
+      hash = 83 * hash + Objects.hashCode(this.wildcardType);
+      hash = 83 * hash + Objects.hashCode(this.wildcardSubtype);
+      return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null) {
+        return false;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      final MediaType other = (MediaType) obj;
+      if (!Objects.equals(this.type, other.type)) {
+        return false;
+      }
+      if (!Objects.equals(this.subtype, other.subtype)) {
+        return false;
+      }
+      if (!Objects.equals(this.parameters, other.parameters)) {
+        return false;
+      }
+      if (!Objects.equals(this.wildcardType, other.wildcardType)) {
+        return false;
+      }
+      return Objects.equals(this.wildcardSubtype, other.wildcardSubtype);
+    }
+  }
+
+  public static class Variant {
+
+    @JsonProperty(value = "language", required = false)
+    public final String language;
+
+    @JsonProperty(value = "mediaType", required = false)
+    public final MediaType mediaType;
+
+    @JsonProperty(value = "encoding", required = false)
+    public final String encoding;
+
+    @JsonProperty(value = "languageString", required = false)
+    public final String languageString;
+
+    @JsonCreator
+    public Variant(
+        @JsonProperty("language") String language,
+        @JsonProperty("mediaType") MediaType mediaType,
+        @JsonProperty("encoding") String encoding,
+        @JsonProperty("languageString") String languageString) {
+      this.language = language;
+      this.mediaType = mediaType;
+      this.encoding = encoding;
+      this.languageString = languageString;
+    }
+
+    @Override
+    public int hashCode() {
+      int hash = 5;
+      hash = 83 * hash + Objects.hashCode(this.language);
+      hash = 83 * hash + Objects.hashCode(this.mediaType);
+      hash = 83 * hash + Objects.hashCode(this.encoding);
+      hash = 83 * hash + Objects.hashCode(this.languageString);
+      return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+      if (obj == null) {
+        return false;
+      }
+      if (getClass() != obj.getClass()) {
+        return false;
+      }
+      final Variant other = (Variant) obj;
+      if (!Objects.equals(this.language, other.language)) {
+        return false;
+      }
+      if (!Objects.equals(this.mediaType, other.mediaType)) {
+        return false;
+      }
+      if (!Objects.equals(this.encoding, other.encoding)) {
+        return false;
+      }
+      return Objects.equals(this.languageString, other.languageString);
+    }
+  }
+}
