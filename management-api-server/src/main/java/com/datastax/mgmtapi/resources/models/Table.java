d0f2e52f2085f11a1acce70b632d80950d4fb48c
Olivier Michallat <olim7t@users.noreply.github.com>
2023-08-23 15:19:00 -0700

    Implement Table.equals & hashCode

--- management-api-server/src/main/java/com/datastax/mgmtapi/resources/models/Table.java
+++ management-api-server/src/main/java/com/datastax/mgmtapi/resources/models/Table.java
@@ -7,7 +7,10 @@ package com.datastax.mgmtapi.resources.models;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Map;
+import java.util.Objects;
 
 public class Table {
   @JsonProperty(value = "name", required = true)
@@ -22,5 +25,31 @@ public class Table {
       @JsonProperty("compaction") Map<String, String> compaction) {
     this.name = name;
     this.compaction = compaction;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Table table = (Table) o;
+    return Objects.equals(name, table.name) && Objects.equals(compaction, table.compaction);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, compaction);
+  }
+
+  @Override
+  public String toString() {
+    try {
+      return new ObjectMapper().writeValueAsString(this);
+    } catch (JsonProcessingException je) {
+      return String.format("Unable to format table (%s)", je.getMessage());
+    }
   }
 }
