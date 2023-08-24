2dfe4aa6a19f798b73cff1444718ed736c9cdf7e
Miles-Garnsey <miles.garnsey@datastax.com>
2023-08-24 18:06:02 +1000

    Fix test.

--- management-api-server/src/test/java/com/datastax/mgmtapi/K8OperatorResourcesTest.java
+++ management-api-server/src/test/java/com/datastax/mgmtapi/K8OperatorResourcesTest.java
@@ -1647,11 +1647,15 @@ public class K8OperatorResourcesTest {
     verify(context.cqlService)
         .executePreparedStatement(
             any(),
-            eq("CALL NodeOps.repair(?, ?, ?, ?)"),
+            eq("CALL NodeOps.repair(?, ?, ?, ?, ?, ?, ?, ?)"),
             eq("test_ks"),
             eq(null),
             eq(true),
-            eq(false));
+            eq(false),
+            eq(Optional.empty()),
+            eq(Optional.empty()),
+            eq(Optional.empty()),
+            eq(Optional.empty()));
   }
 
   @Test
@@ -1688,9 +1692,9 @@ public class K8OperatorResourcesTest {
             any(),
             eq("CALL NodeOps.repair(?, ?, ?, ?, ?, ?, ?, ?)"),
             eq("test_ks"),
-            eq(Optional.empty()),
-            eq(Optional.empty()),
-            eq(Optional.empty()),
+            eq(null),
+            eq(true),
+            eq(true),
             eq(Optional.empty()),
             eq(Optional.empty()),
             eq(Optional.empty()),
