bea833b0383bba1f84f4b6f07fde9bb8845a603f
Miles Garnsey <11435896+Miles-Garnsey@users.noreply.github.com>
2023-08-31 11:44:35 +1000

    Reaper endpoints: Async Repair Endpoint (#358)
    * Allow `repair()` RPC method to take more arguments (as required by Reaper). Get the old NodeOpsResources class calling the new RPC method signature.
    
    ---------
    
    Co-authored-by: Erik Merkle <erik.merkle@datastax.com>
    
    * Cancel repairs endpoint (#368)
    
    * Add cancelAllRepairs endpoint and requisite NodeOps.

---
+++ management-api-server/src/main/java/com/datastax/mgmtapi/resources/v2/models/RingRange.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Please see the included license file for details.
+ */
+
+package com.datastax.mgmtapi.resources.v2.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.math.BigInteger;
+import java.util.Comparator;
+import java.util.Objects;
+
+public final class RingRange {
+  public static final Comparator<RingRange> START_COMPARATOR =
+      (RingRange o1, RingRange o2) -> o1.start.compareTo(o2.start);
+
+  @JsonProperty(value = "start", required = true)
+  public final BigInteger start;
+
+  @JsonProperty(value = "end", required = true)
+  public final BigInteger end;
+
+  public RingRange(
+      @JsonProperty(value = "start", required = true) BigInteger start,
+      @JsonProperty(value = "end", required = true) BigInteger end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  public RingRange(String... range) {
+    start = new BigInteger(range[0]);
+    end = new BigInteger(range[1]);
+  }
+
+  public BigInteger getStart() {
+    return start;
+  }
+
+  public BigInteger getEnd() {
+    return end;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(start) + Objects.hashCode(end);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || !(o instanceof RingRange)) {
+      return false;
+    }
+    RingRange other = (RingRange) o;
+    return Objects.equals(start, other.start) && Objects.equals(end, other.end);
+  }
+}
